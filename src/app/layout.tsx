60d655d4c09a78b19fba9a20bd493811248b6aee
Brenno França <brenno.franca@grupoegb.com>
2026-01-19 23:11:59 -0300

    new: clarity content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import ClientWrapper from "./ClientWrapper";
 
@@ -28,6 +29,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v46ic8v1kp");
+          `}
+        </Script>
+
         <meta name="robots" content="noindex,nofollow,noarchive,nosnippet,noimageindex" />
         <meta name="googlebot" content="noindex,nofollow,noarchive,nosnippet,noimageindex" />
       </head>
